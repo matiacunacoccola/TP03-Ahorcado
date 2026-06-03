af71ea7f68f9108bbd733032574df1f31bdf9383
MATÍAS ACUÑA COCCOLA <50233630@ort>
2026-06-03 08:59:18 -0300

    cambios

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TP3_Ahorcado.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ Models/PalabrasAhorcado.cs
@@ -0,0 +1,19 @@
+public class PalabrasAhorcado()
+{
+   private List<string> palabras;
+
+   public PalabrasAhorcado(list palabras)
+   {
+     palabras=new list<string>
+     {COMPUTADORA,PROGRAMACION,INFORMATICA,LABORATORIO,COMPAÑERO,ARQUITECTURA,SEGMENTACION,PAGINACION,PROCESADOR,TELEVISION}
+
+   }
+
+   public string obtenerPalabra()
+   {
+      return 
+
+   }
+
+
+}
